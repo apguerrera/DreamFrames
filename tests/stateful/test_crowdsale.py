40d74e2f6a6cfaa2dddaa8f2d14ea635e629f8ee
Adrian Guerrera <adrianguerrera@gmail.com>
2020-05-05 18:41:02 +0700

    Added descriptors to state machine testing

--- tests/stateful/test_crowdsale.py
+++ tests/stateful/test_crowdsale.py
@@ -7,7 +7,7 @@ from brownie import accounts, web3, Wei, rpc
 
 class StateMachine:
 
-    st_value = strategy('uint256', max_value="10 ether")
+    st_eth = strategy('uint256', max_value="10 ether")
     st_usd = strategy('uint256', max_value="1000000 ether")
     st_sleep = strategy('uint256', max_value=60000)
 
@@ -22,25 +22,28 @@ class StateMachine:
         # zero the deposit amounts at the start of each test run
         self.contributed_usd = 0
 
-    def rule_buy(self, st_address, st_value):
+    def rule_buy(self, st_address, st_eth):
         # make a deposit and adjust the local record
-        tx = self.contract.calculateEthFrames(st_value,st_address, {'from': st_address})
-        if tx[0] > 0 :
-            self.contract.buyFramesEth({'from': st_address, 'value': st_value})
-            self.contributed_usd += tx[0] * self.contract.frameUsdWithBonus(st_address, {'from': st_address})
+        tx = self.contract.calculateEthFrames(st_eth, st_address, {'from': st_address})
+        frames = tx[0]
+        if frames > 0 :
+            self.contract.buyFramesEth({'from': st_address, 'value': st_eth})
+            self.contributed_usd += frames * self.contract.frameUsdWithBonus(st_address, {'from': st_address})
         else: 
             with brownie.reverts():
-                self.contract.buyFramesEth({'from': st_address, 'value': st_value})
+                self.contract.buyFramesEth({'from': st_address, 'value': st_eth})
 
     def rule_offchain(self, st_address, st_usd):
-        # make a purchase offchain and adjust the local record
+        # convert dollars into number of frames available to purchase
         tx = self.contract.calculateUsdFrames(st_usd, st_address, {'from': accounts[1]})
-        if tx[0] > 0:
-            self.contract.offlineFramesPurchase(st_address,tx[0], {'from': accounts[1]})
-            self.contributed_usd += tx[1]
+        frames = tx[0]
+        usd_to_contribute = tx[1] 
+        if frames > 0:
+            self.contract.offlineFramesPurchase(st_address,frames, {'from': accounts[1]})
+            self.contributed_usd += usd_to_contribute
         else:
             with brownie.reverts():
-                self.contract.offlineFramesPurchase(st_address,tx[0], {'from': accounts[1]})         
+                self.contract.offlineFramesPurchase(st_address,frames, {'from': accounts[1]})         
 
     def rule_sleep(self, st_sleep):
         rpc.sleep(st_sleep)
