0293f50c09b366d28bf23a9303c20b161d9d5a83
sssubik <subikshrestha@gmail.com>
2021-07-03 11:55:00 +0545

    added more test

--- tests/DreamFrames/test_frame_rush.py
+++ tests/DreamFrames/test_frame_rush.py
@@ -16,17 +16,16 @@ def _claimCollectableToken(frame_rush, frame_token,tokenId, _account):
     _account = _account
     frame_token.approve(accounts[5], 100 * TENPOW18, {'from': accounts[0]})
 
-    frame_token.transferFrom(accounts[0], accounts[3], 100 * TENPOW18, {'from':accounts[5]})
-    frame_token.approve(_account, 100 * TENPOW18, {'from': _account})
+    frame_token.transferFrom(accounts[0], _account, 100 * TENPOW18, {'from':accounts[5]})
     frame_token.approve(frame_rush, 100 * TENPOW18, {'from': _account})
     
     
     frame_rush.claimCollectableToken(_account,tokenId, {"from": _account})
     return frame_rush, frame_token
 
+
 def test_frame_rush_claim(frame_rush,collectable_token, frame_token):
     _account = accounts[3]
-    
    # _tokenId = _mint_collectable_token(collectable_token,frame_rush)
     tokenId = 1
     frame_rush, frame_token =  _claimCollectableToken(frame_rush, frame_token,tokenId,_account)
@@ -34,7 +33,24 @@ def test_frame_rush_claim(frame_rush,collectable_token, frame_token):
     tokenId = 2
     frame_rush, frame_token =  _claimCollectableToken(frame_rush, frame_token,tokenId,_account)
     
+
+
+def test_frame_rush_claim_balance_check(frame_rush,collectable_token, frame_token):
+    _account = accounts[3]
+    frame_token.approve(accounts[5], 100 * TENPOW18, {'from': accounts[0]})
+
+    frame_token.transferFrom(accounts[0], _account, 100 * TENPOW18, {'from':accounts[5]})
+    frame_token.approve(frame_rush, 100 * TENPOW18, {'from': _account})
     
+   # _tokenId = _mint_collectable_token(collectable_token,frame_rush)
+    tokenId = 1
+    frame_rush.claimCollectableToken(_account,tokenId, {"from": _account})
+    tokenId = 2
+    frame_rush.claimCollectableToken(_account,tokenId, {"from": _account})
+    
+    assert frame_token.balanceOf(_account) == 50 * TENPOW18
+
+
 
 
 def test_is_token_available(frame_rush,collectable_token, frame_token):
